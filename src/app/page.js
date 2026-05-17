d2e3adce8ec89c29120b7bc5f6cb34bf1def4086
find04744-arch <find04744@gmail.com>
2026-05-17 22:50:26 +0600

    navbar done

--- src/app/page.js
+++ src/app/page.js
@@ -6,16 +6,16 @@ import WorkingProcess from '@/components/home/WorkingProcess';
 export default function Home() {
   return (
     <div className="space-y-20 pb-20">
-      {/* ১. হিরো ব্যানার সেকশন (Swiper.js স্লাইডার) */}
+      
       <HeroBanner />
 
-      {/* ২. টপ রেটেড ডক্টর সেকশন (৩টি ডাইনামিক কার্ড) */}
+     
       <TopDoctors />
 
-      {/* ৩. অতিরিক্ত সেকশন ১: স্পেশালিটি */}
+ 
       <Specialities />
 
-      {/* ৪. অতিরিক্ত সেকশন ২: কাজের ধাপসমূহ */}
+      
       <WorkingProcess />
     </div>
   );

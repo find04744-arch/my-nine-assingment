87444a9bceb3fd7f93c37c51a86450a268cdf31b
find04744-arch <find04744@gmail.com>
2026-05-18 10:30:50 +0600

    fix: remove required constraint from timeSlot

--- src/models/Appointment.js
+++ src/models/Appointment.js
@@ -5,12 +5,11 @@ const AppointmentSchema = new mongoose.Schema(
     patientName: { type: String, required: true },
     email: { type: String, required: true },
     date: { type: String, required: true },
-    timeSlot: { type: String, required: true },
+    timeSlot: { type: String }, 
     doctorId: { type: mongoose.Schema.Types.ObjectId, ref: "Doctor" },
   },
   { timestamps: true }
 );
-
 
 const Appointment = mongoose.models.Appointment || mongoose.model("Appointment", AppointmentSchema);
 
